ef1ae19e8bee3ccb12205374e0cf99c366d1f275
gustavohg <gustavohg@gustavohg-Dell-System-Inspiron-N4110>
2016-02-21 15:29:10 -0600

    Nuevos paquetes.

--- Crazy_Breakout/src/Logic/Logic.java
+++ Crazy_Breakout/src/Logic/Logic.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package crazy_breakout;
+package Logic;
 
 /**
  *
  * @author gustavohg
  */
-public class Crazy_Breakout {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Logic {
     
 }
