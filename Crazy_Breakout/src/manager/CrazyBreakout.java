d22507f5f6194f8c0d033bbd97b2d80755447532
Jacob <jacob.gonga@gmail.com>
2016-02-22 16:41:08 -0600

    Creación paquete manager, que contiene el método main()

---
+++ Crazy_Breakout/src/manager/CrazyBreakout.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manager;
+
+/**
+ *
+ * @author Daniel
+ */
+public class CrazyBreakout {
+    public static void main(String[] args) {
+        
+    
+    }
+    
+}
