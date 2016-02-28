050bbe9db6e0b99f2a1053958e594389aace7802
Jacob <jacob.gonga@gmail.com>
2016-02-27 18:32:16 -0600

    Creación de la interface ConstantesCB , la cual incluye las constantes del programa, de esta manera será mejor el control de estas variables

---
+++ Crazy_Breakout/src/Logic/ConstantesCB.java
@@ -0,0 +1,15 @@
+package Logic;
+
+/**
+ * Interface que declara las variables constantes del programa
+ * 
+ */
+public interface ConstantesCB {
+    public int POS_INICIAL_BOLAX = 0;
+    public int POS_INICIAL_BOLAY = 200;
+    public int POS_INICIAL_BARRAX = 150;
+    public int POS_INICIAL_BARRAY = 200;
+    public int ANCHO_PANTALLA = 200;
+    public int LARGO_PANTALLA = 300;
+    
+}
