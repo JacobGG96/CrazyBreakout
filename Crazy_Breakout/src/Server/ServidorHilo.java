1b6d7fd406e762ac48da588ad82730defd4bbfe3
gustavohg <gustavohg@gustavohg-Dell-System-Inspiron-N4110>
2016-02-29 10:43:14 -0600

    ServidorHilo

--- Crazy_Breakout/src/Server/ServidorHilo.java
+++ Crazy_Breakout/src/Server/ServidorHilo.java
@@ -15,26 +15,15 @@ public class ServidorHilo extends Thread {
     private DataInputStream dis;
     private int idSessio;
     
-    //constructor  que inicializar el socket y asigna  el id al numero de sesi�n 
+    //constructor  que inicializar el socket y asigna  el id al numero de sesion 
     public ServidorHilo(Socket socket, int id) {
         this.socket = socket;
         this.idSessio = id;
         try {
             dos = new DataOutputStream(socket.getOutputStream());
             dis = new DataInputStream(socket.getInputStream());
-        } catch (IOException ex) {
-            Logger.getLogger(ServidorHilo.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    /**
-     * metodo que se   encarga de  cerrar todas las  conexiones  que se  realizaron en el socket  
-     * 
-     * 
-     */
-    public void desconnectar() {
-        try {
-            socket.close();
-        } catch (IOException ex) {
+        } 
+        catch (IOException ex) {
             Logger.getLogger(ServidorHilo.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
@@ -50,29 +39,38 @@ public class ServidorHilo extends Thread {
         while (true){
         	try{
 	            msg = dis.readUTF();
-		    if(msg!=null){
-		        System.out.println("Cliente #"+this.idSessio+" >>> "+msg);
-		        dos.writeUTF("Mensaje recibido");
-		    }
+//		    if(msg!=null){
+//		        System.out.println("Cliente #"+this.idSessio+" >>> "+msg);
+//		        dos.writeUTF("Mensaje recibido");
+//		    }
+                    if(msg.equals("Actualizacion")){
+                        System.out.println("Cliente #"+this.idSessio+" >>> "+msg);
+                        actual();
+                    }
+                    if(msg.equals("Desconectar")){
+                        desconectar();
+                    }
 		}
         	catch(Exception e){
  	       	}
         }
     }
-
-    public void leer(){
-    	String texto =""; 
-    	
-    	try{
-    		FileReader lector = new FileReader("texto.txt");
-    		BufferedReader contenido=new BufferedReader(lector);
-    		while((texto=contenido.readLine())!=null){
-    			System.out.println(texto);
-    		}
-    	}
-    	catch(Exception e){
-    		System.out.println("Error al leer");
-    	}
+    
+    /**
+     * metodo que se   encarga de  cerrar todas las  conexiones  que se  realizaron en el socket  
+     * 
+     * 
+     */
+    private void desconectar() {
+        try {
+            socket.close();
+            System.out.println("Cliente #"+this.idSessio+" >>> Se ha desconectado");
+        } catch (IOException ex) {
+            Logger.getLogger(ServidorHilo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private void actual() throws IOException{
+        dos.writeUTF("Enviando actualización");
     }
 }
-
