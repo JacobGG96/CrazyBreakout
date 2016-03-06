ce0c9d29c31cb9bbdcf84a3e18df8319f94317bb
gustavohg <gustavohg@gustavohg-Dell-System-Inspiron-N4110>
2016-03-06 14:48:01 -0600

    Generación de UI

--- Crazy_Breakout/src/UI/Pantalla.java
+++ Crazy_Breakout/src/UI/Pantalla.java
@@ -1,6 +1,34 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package UI;
+import Logic.ConstantesCB;
+import javax.swing.*;
+import java.awt.*;
 
-public class Pantalla {
+/**
+ *
+ * @author gustavohg
+ */
+public class Pantalla extends JFrame implements ConstantesCB {
+    
+    public Pantalla(){
+        
+        initUI();
+    }
+    
+    public void initUI(){
+        
+        JButton new_game_b = new JButton("Nuevo Juego");
+        
+        
+        setTitle("Crazy Breakout");
+        setSize(LARGO_PANTALLA,ANCHO_PANTALLA);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+    }
     
 }
