0f4083c01659c25ba5636b748da15ac6200afe8b
Jacob <jacob.gonga@gmail.com>
2016-03-09 14:58:12 -0600

    Se eliminaron los métodos getRect

--- Crazy_Breakout/src/Logic/Bloque.java
+++ Crazy_Breakout/src/Logic/Bloque.java
@@ -1,7 +1,9 @@
 package Logic;
 
 import java.awt.Rectangle;
+import java.awt.geom.Line2D;
 import javax.swing.ImageIcon;
+import java.awt.geom.Line2D.Double;
 
 /**
  * Clase para manejo de los bloques
@@ -13,13 +15,10 @@ public class Bloque extends General{
     
     private boolean destruido;
     private int resistencia;
-    private Rectangle rect1;
-    private Rectangle rect2;
-    private Rectangle rect3;
-    private Rectangle rect4;
+    
     private boolean sorpresa;
-    private int sub_ancho;
-    private int sub_alto;
+    //private int sub_ancho;
+    //private int sub_alto;
     
     /**
      * Constructor de la clase, asigna a los bloques una imagen
@@ -29,9 +28,7 @@ public class Bloque extends General{
      */
     public Bloque(int x, int y, int res) {
         
-        sub_ancho = getAncho() / 2;
-        sub_alto = getAlto() / 2;
-        
+                
         this.x = x;
         this.y = y;
         resistencia = res;
@@ -52,15 +49,11 @@ public class Bloque extends General{
     }
 
     public void setResistencia(int daño) {
+        this.resistencia += daño;
         if(this.resistencia == 0){
             setDestruido(true);
         }
-        
-        this.resistencia += daño;
-    }
-    
-    
-    
+    }    
        
     /**
      * Devuelve el estado del bloque
@@ -76,24 +69,6 @@ public class Bloque extends General{
      */
     public void setDestruido(boolean valor) {
         destruido = valor;
-    }
-    
-    public Rectangle getRect1() {
-        return new Rectangle(x, y, sub_ancho , sub_alto);
-    }
-
-    public Rectangle getRect2() {
-        return new Rectangle(x + sub_ancho, y, sub_ancho , sub_alto);
-    }
-
-    public Rectangle getRect3() {
-        return new Rectangle(x, y + sub_alto, sub_ancho , sub_alto);
-    }
-
-    public Rectangle getRect4() {
-        return new Rectangle(x + sub_ancho, y + sub_alto, sub_ancho , sub_alto);
-    }
-    
-    
+    } 
     
 }
